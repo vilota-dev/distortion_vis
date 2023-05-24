1d383d3b83fd636363e9c77a136356fee238996d
Tejas Garrepally <ttejasgarrepally@gmail.com>
2023-05-24 17:27:25 +0800

    added FOV filtering, reject points outside FOV, calculating polar angles

--- distortion_models/pinhole.py
+++ distortion_models/pinhole.py
@@ -1,14 +1,25 @@
+import numpy as np
+
+
 class Pinhole:
     def __init__(self, fx, fy, cx, cy):
         self.fx, self.fy, self.cx, self.cy = fx, fy, cx, cy
+        self.fov = 180
 
     def project(self, points_3D):
+        """ Use FOV to reject invalid points """
         x = points_3D[:, 0]
         y = points_3D[:, 1]
         z = points_3D[:, 2]
+
+        # Reject points that are behind the camera
+        # Calculate polar angle for each of the points
+        polar_angle = np.arctan2(np.sqrt(x**2 + y**2), z)
+        valid = polar_angle < np.deg2rad(self.fov / 2)
+
         x_projected = self.fx * x / z + self.cx
         y_projected = self.fy * y / z + self.cy
-        return x_projected, y_projected
+        return x_projected, y_projected, valid
 
     def unproject(self, points_2D):
         """ Instead of arbitrarily generated a grid of 3D points, we can generate a 2D grid with the resolution given,
