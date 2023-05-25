c96df7e596668cd4f85f92ff86c9d9ade7012f70
Tejas Garrepally <ttejasgarrepally@gmail.com>
2023-05-25 13:56:52 +0800

    changed to multiply by the coefficient to fix projection, only relevant to DS

--- distortion_models/double_sphere.py
+++ distortion_models/double_sphere.py
@@ -5,7 +5,11 @@ import torch
 
 class DoubleSphere(Pinhole):
     def __init__(self, fx, fy, cx, cy, xi, alpha, fov: float = 180):
-        super().__init__(fx, fy, cx, cy)
+        self.fx_original = fx
+        self.fy_original = fy
+        fx_adj = fx * (1 - alpha)
+        fy_adj = fy * (1 - alpha)
+        super().__init__(fx_adj, fy_adj, cx, cy)
         self.xi, self.alpha = xi, alpha
         # Below parameters are not used for now.
         self.fov = fov
@@ -23,7 +27,29 @@ class DoubleSphere(Pinhole):
 
         denominator = self.alpha * d2 + (1 - self.alpha) * (self.xi * d1 + z)
 
-        u = self.fx * x / denominator + self.cx
-        v = self.fy * y / denominator + self.cy
+        u = self.fx_original * x / denominator + self.cx
+        v = self.fy_original * y / denominator + self.cy
 
         return torch.hstack((u.reshape(-1, 1), v.reshape(-1, 1)))
+
+    def cam2world(self, points_2D):
+        u, v = points_2D.T
+
+        mx = (u - self.cx) / self.fx
+        my = (v - self.cy) / self.fy
+        r_2 = mx ** 2 + my ** 2
+        mz = (1 - (self.alpha ** 2) * r_2) / \
+             (self.alpha * torch.sqrt(1 - (2 * self.alpha - 1) * r_2) +
+              (1 - self.alpha))
+
+        coefficient = (mz * self.xi +
+                       torch.sqrt(mz ** 2 + (1 - self.xi ** 2) * r_2)) / \
+                      (mz ** 2 + r_2)
+
+        x = coefficient * mx
+        y = coefficient * my
+        z = coefficient * mz - self.xi
+
+        return torch.hstack((x.reshape(-1, 1),
+                             y.reshape(-1, 1),
+                             z.reshape(-1, 1)))
