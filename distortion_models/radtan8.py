32c45cf42fe1faf51bc4f7a625506ff9334f3e78
Tejas Garrepally <ttejasgarrepally@gmail.com>
2023-05-29 16:10:25 +0800

    added imports

--- distortion_models/radtan8.py
+++ distortion_models/radtan8.py
@@ -9,7 +9,7 @@ class RadTan8(Pinhole):
         self.cx, self.cy = cx, cy
         self.k1, self.k2, self.p1, self.p2, self.k3, self.k4, self.k5, self.k6, self.rpmax = k1, k2, p1, p2, k3, k4, k5, k6, rpmax
         self.fov_radtan8 = 180
-        
+
     def __str__(self):
         return "Radial Tangential 8 (radtan8)"
 
